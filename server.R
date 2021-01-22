dc197958ad5e2630e1aa7f2a3b08b69909f2582d
Charm <abaxaci15@users.noreply.github.com>
2021-01-22 15:35:12 -0500

    Add files via upload

---
+++ server.R
@@ -0,0 +1,50 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+
+library(shiny)
+library(shiny)
+library(leaflet)
+library(dplyr)
+library(plyr)
+
+mapdata<-read.csv("mapdata.csv")
+
+for (i in 1:nrow(mapdata))
+{  if  (is.na(mapdata$amount[i])) {mapdata$valuerange[i]<-"NA"}
+    else if (as.numeric(mapdata$amount[i])>=100000 & as.numeric(mapdata$amount[i])<200000) {mapdata$valuerange[i]<-"$100,000 to $199,999"}
+    else if (as.numeric(mapdata$amount[i])>=200000 & as.numeric(mapdata$amount[i])<300000) {mapdata$valuerange[i]<-"$200,000 to $299,999"}
+    else if (as.numeric(mapdata$amount[i])>=300000 & as.numeric(mapdata$amount[i])<400000) {mapdata$valuerange[i]<-"$300,000 to $399,999"}
+    else if  (as.numeric(mapdata$amount[i])>400000)  {mapdata$valuerange[i]<-"$400,000+"}
+    else {mapdata$valuerange[i]<-"NA"}
+}
+
+for (i in 1:nrow(mapdata))
+{  if  (is.na(mapdata$year[i])) {mapdata$yearrange[i]<-"NA"}
+    else if (as.numeric(mapdata$year[i])>=1800 & as.numeric(mapdata$year[i])<1900) {mapdata$yearrange[i]<-"Before 1900"}
+    else if (as.numeric(mapdata$year[i])>=1900 & as.numeric(mapdata$year[i])<1921) {mapdata$yearrange[i]<-"1901 to 1920"}
+    else if (as.numeric(mapdata$year[i])>=1921 & as.numeric(mapdata$year[i])<1941) {mapdata$yearrange[i]<-"1921 to 1940"}
+    else if  (as.numeric(mapdata$year[i])>=1941)  {mapdata$yearrange[i]<-"1941 to 1952"}
+    else {mapdata$yearrange[i]<-"NA"}
+}
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output){
+    
+    observeEvent(input$submit, {
+        
+     num_bed<- mapdata %>% filter(valuerange %in% input$value)
+     output$r1map <- renderLeaflet({
+     leaflet(num_bed) %>%
+     addTiles() %>%  # Add default OpenStreetMap map tiles
+     addMarkers(lng=as.numeric(num_bed$longitude), lat=as.numeric(num_bed$latitude), 
+     popup=paste0("Address:",num_bed$street,"<br>","Zillow Home Value Estimate:",num_bed$amount))
+    }) 
+        
+ }) })
---
+++ ui.R
@@ -0,0 +1,45 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+
+
+
+library(leaflet)
+
+
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    
+    
+    # Application title
+    titlePanel("Home Price Ranges of Homes in Baltimore's Mayfield Neighborhood"),
+        h3("Select Price Range from Drop-Down to Populate Map of Homes"),
+        
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+           #  selectInput("bedrooms",
+            #            "Number of Bedrooms:",
+             #           choices=c("1","2","3","4","5","6")),
+           #  selectInput("bathrooms",
+            #             "Number of Bathrooms:",
+             #            choices=c('1.0','1.5','2.0','3.0','3.5','4.0','4.5')),
+            # selectInput("years",
+             #            "Year Built:",
+              #           choices=c('Before 1900', '1901 to 1920', '1921 to 1940', '1941 to 1952')),
+             selectInput("value","Zillow Home Estimate Range:", 
+                         choices=c("$100,000 to $199,999","$200,000 to $299,999","$300,000 to $399,999","$400,000+")),
+              actionButton("submit","Submit")          
+             ),
+                # Show a plot of the generated distribution
+        mainPanel(
+            leafletOutput("r1map", width="605", height="650")
+        )
+    )))
+
